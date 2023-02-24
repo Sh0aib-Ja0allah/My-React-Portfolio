8dc71421dc8d2366dd80caee27e85ef653d7d164
Che S Jadallah <110256562+Sh0aib-Ja0allah@users.noreply.github.com>
2023-02-24 14:23:14 +0200

    Add files via upload

---
+++ build/precache-manifest.724ee3ab5824aff451a11535a1340a0c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3f2349d273cdf175f7",
+    "url": "/reactportfolio/static/css/main.fbd60f78.chunk.css"
+  },
+  {
+    "revision": "bf3f2349d273cdf175f7",
+    "url": "/reactportfolio/static/js/main.b3775f26.chunk.js"
+  },
+  {
+    "revision": "e921a9a0268651837613",
+    "url": "/reactportfolio/static/js/runtime~main.50cea298.js"
+  },
+  {
+    "revision": "5996bb813e2951abf845",
+    "url": "/reactportfolio/static/js/2.939b4074.chunk.js"
+  },
+  {
+    "revision": "b8002e3123a0f2bc517b",
+    "url": "/reactportfolio/static/js/3.240d59fc.chunk.js"
+  },
+  {
+    "revision": "8ae174f8a4c6892aba6515b3991da136",
+    "url": "/reactportfolio/static/media/About.8ae174f8.jpg"
+  },
+  {
+    "revision": "25eed615e96b223b0d64d8ea57584685",
+    "url": "/reactportfolio/static/media/sho3la.25eed615.png"
+  },
+  {
+    "revision": "db00a99dc8d20ace52e0fd868f3acdca",
+    "url": "/reactportfolio/static/media/senior.db00a99d.png"
+  },
+  {
+    "revision": "464fc10eddb30ad12415058519822809",
+    "url": "/reactportfolio/static/media/gym.464fc10e.png"
+  },
+  {
+    "revision": "1a7f59af6f140b85a60a11fea4f3568c",
+    "url": "/reactportfolio/static/media/practice.1a7f59af.png"
+  },
+  {
+    "revision": "2c9ee196c165654dde11ac58a5e248c1",
+    "url": "/reactportfolio/static/media/lightup.2c9ee196.png"
+  },
+  {
+    "revision": "b41e4a5d3c42e46fab3028b5658c22f4",
+    "url": "/reactportfolio/static/media/shertee.b41e4a5d.png"
+  },
+  {
+    "revision": "737bda74d47b5588b018f06ab77654db",
+    "url": "/reactportfolio/static/media/smart.737bda74.png"
+  },
+  {
+    "revision": "2389e31055915b082b79a932ed76406c",
+    "url": "/reactportfolio/static/media/portfolio1.2389e310.png"
+  },
+  {
+    "revision": "4994879d2de4aecaac9c793aae0c816d",
+    "url": "/reactportfolio/static/media/portfolio2.4994879d.png"
+  },
+  {
+    "revision": "052bda3b7722491ea5fce56c20833b01",
+    "url": "/reactportfolio/static/media/Background.052bda3b.jpg"
+  },
+  {
+    "revision": "c3990b1171e82a4f70294a9ce706f949",
+    "url": "/reactportfolio/index.html"
+  }
+];
